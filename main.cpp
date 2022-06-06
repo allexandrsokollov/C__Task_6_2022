a432c28bec2ab1403b9c88e02663b9c8b50225b4
Alexandr <allexandersokollov@gmail.com>
2022-06-06 07:52:35 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
-#include "User.cpp"
-
 using namespace std;
+
+struct User {
+    User *next;
+    bool Case;
+    // true - Admin false - Customer
+    char name[256];
+    union {
+        struct {
+            int id;
+            char position[256];
+            char department[256];
+        } Admin;
+
+        struct {
+            int customerID;
+        } Customer;
+    };
+};
+
+User *list = NULL;
 
 void printUser(User *user) {
     cout << "User info below -------------------------- User info below" << endl
@@ -18,36 +36,34 @@ void printUser(User *user) {
     cout << "User info atop -------------------------- User info atop" << endl << endl << endl;
 }
 
-User getNewUserFromConsole() {
-    User newUser{};
+User* getNewUserFromConsole() {
+    User *newUser = new User();
 
-    cout << "input 0 if user is admin end 1 if user is customer";
-    cin >> newUser.Case;
+    cout << "input 1 if user is admin end 0 if user is customer";
+    cin >> newUser->Case;
 
     cout << "Enter name:   ";
-    cin >> newUser.name;
+    cin >> newUser->name;
 
-    if (!newUser.Case) {
+    if (newUser->Case) {
         cout << "Enter id:   " << endl;
-        cin >> newUser.Admin.id;
+        cin >> newUser->Admin.id;
 
         cout << "Enter position:   " << endl;
-        cin >> newUser.Admin.position;
+        cin >> newUser->Admin.position;
 
         cout << "Enter department:   " << endl;
-        cin >> newUser.Admin.department;
+        cin >> newUser->Admin.department;
     } else {
 
         cout << "Enter customer ID:   " << endl;
-        cin >> newUser.Customer.customerID;
+        cin >> newUser->Customer.customerID;
     }
-
-    newUser.next = NULL;
 
     return newUser;
 }
 
-User* getUserViaIndex(User *list, int index) {
+User* getUserViaIndex(int index) {
     User *iter = list;
     while (iter != NULL && index--) {
         iter = iter->next;
@@ -55,20 +71,23 @@ User* getUserViaIndex(User *list, int index) {
     return iter;
 }
 
-void addUserToList(User *list, User userToAdd) {
+void addUserToList() {
+
+    User *temp = getNewUserFromConsole();
     if (list == NULL) {
-        list = new User(userToAdd);
-        list->next = NULL;
+
+        temp->next = list;
+        list = temp;
     } else {
         User *iter = list;
         while (iter->next != NULL) {
             iter = iter->next;
         }
-        iter->next = new User(userToAdd);
+        iter->next = temp;
     }
 }
 
-void printUserList(User *list) {
+void printUserList() {
     User *iter = list;
 
     while (iter != NULL) {
@@ -77,7 +96,7 @@ void printUserList(User *list) {
     }
 }
 
-void deleteViaIndex(User *list, int index) {
+void deleteViaIndex(int index) {
     User *iter;
 
     if (list == NULL) return;
@@ -99,10 +118,7 @@ void deleteViaIndex(User *list, int index) {
     }
 }
 
-
 int main() {
-    User *list = NULL;
-
     while (true) {
         int action;
         cout << "DataBase actions:           (input appropriate number)" << endl
@@ -120,17 +136,15 @@ int main() {
                 int index;
                 cin >> index;
 
-                printUser(getUserViaIndex(list, index));
+                printUser(getUserViaIndex(index));
                 break;
             }
             case 2: {
-                printUserList(list);
+                printUserList();
                 break;
             }
             case 3: {
-                User newUsr = getNewUserFromConsole();
-
-                addUserToList(list, newUsr);
+                addUserToList();
                 break;
             }
             case 4: {
@@ -138,7 +152,7 @@ int main() {
                 cout << "input position to delete user" << endl;
                 cin >> index;
 
-                deleteViaIndex(list, index);
+                deleteViaIndex(index);
                 break;
             }
             case 5: {
